0b1182e47a5b4c619796f2d58caa51b7df199e65
PatrickKann <52541434+PatrickKann@users.noreply.github.com>
2019-08-15 11:42:51 -0300

    Add files via upload

---
+++ assets/data/truckData.ts
@@ -0,0 +1,7 @@
+import { Truck } from '../../app/Truck';
+ 
+export const truckData: Truck[] = [
+ {make: 'm1',  style: 'styleA', engine: 'engineA'},
+ {make: 'm2',  style: 'styleB', engine: 'engineB'},
+ {make: 'm3',  style: 'styleC', engine: 'engineC'}
+]
